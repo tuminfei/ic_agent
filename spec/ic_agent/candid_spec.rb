aee5a4a2236c3271de4d6a08fc4ccaaec919c6f7
Terry.Tu <tuminfei1981@gmail.com>
2023-04-26 15:02:27 +0800

    add rspec

--- spec/ic_agent/candid_spec.rb
+++ spec/ic_agent/candid_spec.rb
@@ -42,4 +42,34 @@ describe IcAgent::Candid do
     data = IcAgent::Candid.encode(params)
     expect(data).to eql("4449444c000178000064a7b3b6e00d")
   end
+
+  it "Int IcAgent::Candid.encode" do
+    params = [{'type': IcAgent::Candid::BaseTypes.int, 'value': 10}]
+    data = IcAgent::Candid.encode(params)
+    expect(data).to eql("4449444c00017c0a")
+  end
+
+  it "Int32 IcAgent::Candid.encode" do
+    params = [{'type': IcAgent::Candid::BaseTypes.int32, 'value': 2147483647}]
+    data = IcAgent::Candid.encode(params)
+    expect(data).to eql("4449444c000175ffffff7f")
+  end
+
+  it "Int64 IcAgent::Candid.encode" do
+    params = [{'type': IcAgent::Candid::BaseTypes.int64, 'value': 1000000000000000000}]
+    data = IcAgent::Candid.encode(params)
+    expect(data).to eql("4449444c000174000064a7b3b6e00d")
+  end
+
+  it "Float32 IcAgent::Candid.encode" do
+    params = [{'type': IcAgent::Candid::BaseTypes.float32, 'value': 42949672.0}]
+    data = IcAgent::Candid.encode(params)
+    expect(data).to eql("4449444c0001730ad7234c")
+  end
+
+  it "Float64 IcAgent::Candid.encode" do
+    params = [{'type': IcAgent::Candid::BaseTypes.float64, 'value': 42949672.0}]
+    data = IcAgent::Candid.encode(params)
+    expect(data).to eql("4449444c00017200000040e17a8441")
+  end
 end
