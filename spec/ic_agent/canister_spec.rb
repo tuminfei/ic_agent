106b1d7d1ef826a0f0b22e0928ea36a5cdbebe7c
Terry.Tu <tuminfei1981@gmail.com>
2023-05-17 23:19:57 +0800

    add define_method

--- spec/ic_agent/canister_spec.rb
+++ spec/ic_agent/canister_spec.rb
@@ -7,6 +7,7 @@ describe IcAgent::Canister do
     client = IcAgent::Client.new
     @agent = IcAgent::Agent.new(iden, client)
     @subnet_key = '308182301d060d2b0601040182dc7c0503010201060c2b0601040182dc7c05030201036100b31b406c9f6648695a88154ae2e4f5fe87883d4ad81c2844c5571b2d91d401cdd40836e763a7c18dccb84629b0d808f7142c3175bc8231dc09bd53637efd6f2568801385ec973d34e6eef9c8c8280a9f4a114163a43a8540941ba367f0c7cb28'
+    @gov_canister_id = 'rrkah-fqaaa-aaaaa-aaaaq-cai'
     @gov_didl = <<~DIDL_DOC
       service : (Governance) -> {
         claim_gtc_neurons : (principal) -> (Result) query;
@@ -29,9 +30,13 @@ describe IcAgent::Canister do
     DIDL_DOC
   end
 
-  it 'IcAgent::Canister call' do
+  it 'didl factory' do
     parser = IcAgent::Ast::Parser.new
     parser.parse(@gov_didl)
+  end
+
+  it 'IcAgent::Canister call' do
+    gov = IcAgent::Canister.new(@agent, @gov_canister_id, @gov_didl)
   end
 end
 
